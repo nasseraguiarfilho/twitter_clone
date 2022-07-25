b6035c1d14027e3bcb43a8b2a2ebc1549f5919ac
Pedro Nasser <nasseraguiarfilho@gmail.com>
2022-07-25 16:41:43 -0600

    updated dateformat for a more user friendly one

--- get_tweet.php
+++ get_tweet.php
@@ -16,7 +16,7 @@ $user = $_SESSION['usuario'];
 $userId = $db -> select("SELECT id FROM usuarios WHERE usuario = $user");
 $userId = $userId[0]['id'];
 
-$sql = "SELECT t.data_inclusao, t.tweet, u.usuario FROM tweet AS t JOIN usuarios AS u ON (t.id_usuario = u.id) WHERE id_usuario = $userId ORDER BY data_inclusao DESC";
+$sql = "SELECT DATE_FORMAT(t.data_inclusao, '%b %d %Y %T') AS dt, t.tweet, u.usuario FROM tweet AS t JOIN usuarios AS u ON (t.id_usuario = u.id) WHERE id_usuario = $userId ORDER BY data_inclusao DESC";
 
 $result = $db -> select($sql);
 
@@ -24,7 +24,7 @@ $result = $db -> select($sql);
 //populating timeline with the tweets on database
  for ($i=0; $i < count($result); $i++) { 
     echo '<a href="#" class="list-group-item"> ';
-        echo '<h4 class="list-group-item-heading">'.$result[$i]['usuario'].' <small>'.$result[$i]['data_inclusao'].'</small> </h4> ';
+        echo '<h4 class="list-group-item-heading">'.$result[$i]['usuario'].' <small>'.$result[$i]['dt'].'</small> </h4> ';
         echo '<p class="list-group-item-text">'.$result[$i]['tweet'].'</p>';
     echo '</a>';
  }
