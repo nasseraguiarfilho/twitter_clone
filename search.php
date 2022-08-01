773db4c4dc0fadbad2d9fb2abad4fd683e2186e9
Pedro Nasser <nasseraguiarfilho@gmail.com>
2022-07-31 20:42:26 -0600

    Search button with added features and 1st contact with search.php

--- search.php
+++ search.php
@@ -1,18 +1,5 @@
 <?php
 
-session_start();
-if (!isset($_SESSION['usuario'])) {
-    header('Location: index.php');
-}
-
-require_once 'Db.php';
-
-$db = new Db();
-$db->connect();
-
-$user = $_SESSION['usuario'];
-
-echo '<a href="#" class="list-group-item"> '. $_POST['find']. '</a>';
-
+echo "hello";
 
 ?>
